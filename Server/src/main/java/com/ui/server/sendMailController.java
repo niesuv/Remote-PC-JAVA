beaad7f4ce71d0cef33683972f06f7363668603c
vcmt794 <128240027+vcmt794@users.noreply.github.com>
2023-11-11 19:30:49 +0700

    Update sendMailController.java

--- Server/src/main/java/com/ui/server/sendMailController.java
+++ Server/src/main/java/com/ui/server/sendMailController.java
@@ -69,6 +69,12 @@ public class sendMailController {
                         sendBut.setDisable(false);
                         addField.setEditable(true);
                     }
+                    else if(t1.equals("ListProcess")){
+                        addField.setDisable(true);
+                        addField.setEditable(false);
+                        labelAddField.setText("Nothing here");
+                        sendBut.setDisable(false);
+                    }
                     else {
                         sendBut.setDisable(true);
                     }
@@ -123,7 +129,6 @@ public class sendMailController {
             });
             thread.start();
 
-
         }
         //key log task
         else if (choose.equals("KeyLogger")) {
@@ -172,6 +177,29 @@ public class sendMailController {
                     System.out.println(Thread.currentThread().getName());
                     SendMail.getInstance().sendMail("req / " + id + " / " + "Shutdown/"+sudopass);
                     logText("ID: " + id +"Password: "+sudopass+ " Send Mail Successfully! If there is a response, It means an error has occured!", "green");
+                } catch (IOException | MessagingException e) {
+                    logText(e.getMessage(), "red");
+                    e.printStackTrace();
+                }
+            });
+            thread.start();
+        } else if (choose.equals("ListProcess")) {
+            int id = random.nextInt(1000,9999);
+            sendBut.setDisable(true);
+            Thread thread = new Thread(() -> {
+                try {
+                    System.out.println(Thread.currentThread().getName());
+                    SendMail.getInstance().sendMail("req / " + id + " / " + "ListProcess");
+                    logText("ID: " + id + " Send Mail Successfully! Wait for response!", "green");
+                    String a = CheckMail.getInstance().listen(id, 40);
+                    if (a != null) {
+                        logText("Get Response successfully. File in " + a, "green");
+                        Platform.runLater(() -> {
+                            sendBut.setDisable(false);
+                        });
+                    } else {
+                        logText("Error happens", "red");
+                    }
                 } catch (IOException | MessagingException e) {
                     logText(e.getMessage(), "red");
                     e.printStackTrace();
