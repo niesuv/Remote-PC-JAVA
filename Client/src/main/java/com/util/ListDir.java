0cc9af6d2b2cfaabd550e7d1cf24b2a04fdf4f96
pnngnas <huusangvtvip@gmail.com>
2023-11-13 22:29:29 +0700

    add

--- Client/src/main/java/com/util/ListDir.java
+++ Client/src/main/java/com/util/ListDir.java
@@ -1,22 +1,65 @@
 package com.util;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class ListDir {
-    private static ListDir instance = new ListDir();
-    private ListDir() {
+    private Path root;
+    private BufferedWriter writer;
+
+    public ListDir(String r) throws IOException {
+        root = Path.of(r).toAbsolutePath().toRealPath();
 
     }
 
-    public String list(String _p) {
-        Path path = Path.of(_p);
-        Files.walkFileTree()
-
-    }
-    public static ListDir getInstance() {
-        return instance;
+    public void listAll(String out) throws IOException {
+        writer = new BufferedWriter(new FileWriter(out));
+        list(root, "");
+        writer.close();
     }
 
+    public void list(Path r, String prefix) throws IOException {
+        if (r.equals(root)) {
+            writer.write(root.toString());
+            writer.newLine();
+        }
+        Stream<Path> stream = Files.list(r);
+        Path[] ls = stream.toArray(Path[]::new);
+        stream.close();
+        for (int i = 0; i < ls.length; i++) {
+            Path p = ls[i];
+            if (i == ls.length - 1) {
+                writer.write(prefix);
+                writer.write("└───");
+                writer.write(p.getName(p.getNameCount() - 1).toString());
+                writer.newLine();
+                writer.flush();
+                if (Files.isDirectory(p))
+                    list(p, prefix + "   ");
+            } else {
+                writer.write(prefix);
+                writer.write("├───");
+                writer.write(p.getName(p.getNameCount() - 1).toString());
+                writer.newLine();
+                writer.flush();
+                if (Files.isDirectory(p))
+                    list(p, prefix + "│   ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            ListDir list = new ListDir("C:\\Users\\huusa");
+            list.listAll("out.txt");
+        } catch (IOException e) {
+            System.out.println("err");
+            e.printStackTrace();
+        }
+    }
 
 }
