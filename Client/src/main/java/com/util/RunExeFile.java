fcf5181c5e4b63b34316ff0992e3f97097de9dce
vcmt794 <128240027+vcmt794@users.noreply.github.com>
2023-11-22 22:41:00 +0700

    Update RunExeFile.java

--- Client/src/main/java/com/util/RunExeFile.java
+++ Client/src/main/java/com/util/RunExeFile.java
@@ -2,15 +2,16 @@ package com.util;
 import java.io.IOException;
 
 public class RunExeFile {
-    public static void runExecutable(String pathToExe) {
+
+    public static int runExecutable(String pathToExe) {
         try {
             ProcessBuilder processBuilder = new ProcessBuilder(pathToExe);
             Process process = processBuilder.start();
             int exitCode = process.waitFor();
-            System.out.println("Exit code: " + exitCode);
+            return exitCode;
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+            return -1;
         }
     }
 }
-
