ca712bc38ebbc589700d3791dd1ca7662e1ba910
vcmt794 <128240027+vcmt794@users.noreply.github.com>
2023-12-10 18:39:51 +0700

    Update ListExeFiles.java

--- Client/src/main/java/com/util/ListExeFiles.java
+++ Client/src/main/java/com/util/ListExeFiles.java
@@ -1,22 +1,25 @@
 package com.util;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class ListExeFiles {
     private static String[] AllprogramFilesPath = {System.getenv("ProgramFiles"), System.getenv("ProgramFiles(x86)")};
-
+   
+    public static String[] cmd = new String[]{"/bin/bash", "-c", "system_profiler SPApplicationsDataType"};
     private static void writeExePathsToFile(String outputPath, String folder) {
+        String os = System.getProperty("os.name").toLowerCase();
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath))) {
-            for (String programFilesPath : AllprogramFilesPath) {
-                if (programFilesPath != null) {
-                    writeExePaths(new File(programFilesPath), writer, folder);
+            if (os.contains("win")) {
+                for (String programFilesPath : AllprogramFilesPath) {
+                    if (programFilesPath != null) {
+                        writeExePaths(new File(programFilesPath), writer, folder);
+                    }
                 }
+            } else if (os.contains("nix") || os.contains("nux") || os.contains("mac")) {
+                writeAppPaths(writer);
             }
-        } catch (IOException e) {
-            handleIOException(e);
+        } catch (IOException e) { e.printStackTrace();} catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -27,23 +30,38 @@ public class ListExeFiles {
             for (File file : files) {
                 if (file.isDirectory()) {
                     writeExePaths(file, writer, folder);
-                } else if (file.isFile() && file.getName().toLowerCase().endsWith(".exe")) {
+                } else if (file.isFile() && (file.getName().toLowerCase().endsWith(".exe"))) {
                     String filePath = file.getAbsolutePath();
                     if (filePath.contains(folder)) {
                         try {
                             writer.write(filePath);
                             writer.newLine();
-                        } catch (IOException e) {
-                            handleIOException(e);
-                        }
+                        } catch (IOException e) {e.printStackTrace();}
                     }
                 }
             }
         }
     }
 
-    private static void handleIOException(IOException e) {
-        e.printStackTrace();
+    private static void writeAppPaths( BufferedWriter writer) throws IOException, InterruptedException {
+        try {
+            Process p = Runtime.getRuntime().exec(cmd);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("    ") && !line.startsWith("      ")) {
+                    writer.write(line);
+                }
+                if (line.contains("Location")) {
+                    writer.write(line);
+                    writer.newLine();
+                }
+            }
+            p.waitFor();
+        }catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public static void main(String[] args) {
